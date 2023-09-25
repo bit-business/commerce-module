e743e9862f1ae326533730bbcb5c51f8fafe3229
bit-business <101217799+bit-business@users.noreply.github.com>
2023-09-25 05:58:56 +0200

    1

--- .gitpod/app.php
+++ .gitpod/app.php
@@ -51,9 +51,12 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
+    /*
 
     'url' => env('APP_URL', 'http://localhost'),
+    */
 
+    'url' => env('APP_URL', 'https://baza-dev.hzuts.hr'),
     'asset_url' => env('ASSET_URL', null),
 
     /*
@@ -67,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'UTC+2',
 
     /*
     |--------------------------------------------------------------------------
@@ -80,7 +83,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'hr',
 
     /*
     |--------------------------------------------------------------------------
